377a20a1419347cbf13360e0184827db56d92719
meelkonline <olivier.verneau@meelk.academy>
2024-07-12 12:08:28 +0200

    improve speech viseme

--- test/mistral.py
+++ test/mistral.py
@@ -7,6 +7,6 @@ login("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")
 messages = [
     {"role": "user", "content": "Tu es un noble Français, tu parles de manière très châtiée. Qui es-tu ?"},
 ]
-chatbot = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3", max_new_tokens=4096)
-output = chatbot(messages)
-print(output)
+# chatbot = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3", max_new_tokens=4096)
+# output = chatbot(messages)
+# print(output)
