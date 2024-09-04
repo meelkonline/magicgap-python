c89876e99bc723cce2af4c1bd3e8cf3d8830b68f
meelkonline <olivier.verneau@meelk.academy>
2024-09-04 17:36:33 +0200

    login hf

--- llama_functions.py
+++ llama_functions.py
@@ -1,14 +1,15 @@
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from huggingface_hub import login
 
 from api_requests import ChatRequest
 
 login("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")
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct", torch_dtype=torch.float16).to("cuda")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device_map="auto")
 
 
 def llama_ask(request: ChatRequest):
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device_map="auto")
     response = pipe(request.messages, max_new_tokens=150)
     return response
