3495e8dc909ed7c64c4eda8799b2112d5257ee3b
meelkonline <olivier.verneau@meelk.academy>
2024-09-04 17:06:20 +0200

    login hf

--- llama_functions.py
+++ llama_functions.py
@@ -1,12 +1,14 @@
+import huggingface
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from huggingface_hub import login
 
 from api_requests import ChatRequest
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
 
+login("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")
 
-# Function to format messages into a single string
 
 def llama_ask(request: ChatRequest):
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device_map="auto")
