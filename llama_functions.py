da683ecb0365c3507b4901ab7151c4aa9ff99d2c
meelkonline <olivier.verneau@meelk.academy>
2024-09-04 17:20:13 +0200

    login hf

--- llama_functions.py
+++ llama_functions.py
@@ -3,10 +3,9 @@ from huggingface_hub import login
 
 from api_requests import ChatRequest
 
+login("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")
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
-
-login("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")
 
 
 def llama_ask(request: ChatRequest):
